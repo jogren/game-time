671fe88b4ad63583ef0c2ed0a65bf5c12785d1cb
davidagitlen <davidagitlen@gmail.com>
2019-07-17 18:13:11 -0600

    Add sass npm and modify Game to include an extra survey, set sample gradients for background and text

--- src/Game.js
+++ src/Game.js
@@ -31,13 +31,12 @@ class Game {
   pickSurveys() {
     let randomIds = [];
     for (let i = 0; i < 15; i++) {
-      let randomId = Math.floor(Math.random() * Math.floor(15) + 1)
-      randomIds.push(randomId)
+      let randomId = Math.floor(Math.random() * Math.floor(15) + 1);
+      randomIds.push(randomId);
     } 
-    this.gameIds = Array.from(new Set(randomIds)).slice(0, 3)
-
+    this.gameIds = Array.from(new Set(randomIds)).slice(0, 4);
     return this.gameIds.map(id => {
-      return this.allSurveys.find(survey => survey.id === id)
+      return this.allSurveys.find(survey => survey.id === id);
     })
   }
 
